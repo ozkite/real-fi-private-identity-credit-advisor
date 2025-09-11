51fac51cdfa9d272c3741cbf960ffd8e1f4107db
psofiterol <gpentafragkas@protonmail.com>
2025-09-11 14:26:59 +0300

    chore: add r pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,15 @@ export default function RootLayout({
         src="https://cloud.umami.is/script.js"
         data-website-id="4f63bc18-938c-46f0-a0c1-873a43098e28"
       />
+      <Script
+        id="reddit-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_hj5tly86ywhe');rdt('track', 'PageVisit');
+          `,
+        }}
+      />
       <ServiceWorkerRegistration />
     </html>
   );
